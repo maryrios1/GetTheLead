aec84c257d5b2792240136618e7a246b24699d95
mary <mary@slave2>
2015-11-19 02:26:12 -0600

    opennlt

---
+++ src/main/java/org/smarttechie/servlet/ClassifyTweets.java
@@ -0,0 +1,165 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.smarttechie.servlet;
+
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.ResultSet;
+import com.datastax.driver.core.Row;
+import com.datastax.driver.core.Session;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import opennlp.tools.doccat.DoccatModel;
+import opennlp.tools.doccat.DocumentCategorizerME;
+import opennlp.tools.doccat.DocumentSampleStream;
+import opennlp.tools.tokenize.Tokenizer;
+import opennlp.tools.tokenize.TokenizerME;
+import opennlp.tools.tokenize.TokenizerModel;
+import opennlp.tools.util.ObjectStream;
+import opennlp.tools.util.PlainTextByLineStream;
+
+/**
+ *
+ * @author mary
+ */
+public class ClassifyTweets extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    
+    DoccatModel model;
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        Cluster cluster;
+        Session session;
+        cluster = Cluster.builder().addContactPoint("localhost").build();
+        session = cluster.connect("GetTheLead");
+        String message ="";
+        trainModel();
+        ResultSet results = session.execute("SELECT * FROM tweets LIMIT 10");
+        for (Row row : results) {
+            System.out.println(
+                    row.getString("user")+", "+row.getString("message"));
+            
+            message = row.getString("message");
+            classifyNewTweet(message);
+            /*
+            try{
+            InputStream is = new FileInputStream(
+                    "/home/mary/Codes/GetTheLeadMaven/src/main/models/en-token.bin");
+ 
+            TokenizerModel model = new TokenizerModel(is);
+
+            Tokenizer tokenizer = new TokenizerME(model);
+            
+            String tokens[] = tokenizer.tokenize(message);
+                System.out.println("");
+            for (String a : tokens)
+                    System.out.println(a);
+
+            is.close();
+            }
+            catch(Exception ex1){
+                System.out.println("ERROR: " + ex1.getMessage().toString());
+            }
+            */
+        
+        }
+        
+        
+    
+    }
+    
+    public void trainModel() {
+        InputStream dataIn = null;
+        try {
+            dataIn = new FileInputStream(
+                    "/home/mary/Codes/GetTheLeadMaven/src/main/dataTraining/tweets.txt");
+            ObjectStream lineStream = new PlainTextByLineStream(dataIn, "UTF-8");
+            ObjectStream sampleStream = new DocumentSampleStream(lineStream);
+            // Specifies the minimum number of times a feature must be seen
+            int cutoff = 2;
+            int trainingIterations = 30;
+            model = DocumentCategorizerME.train("en", sampleStream, cutoff,
+                    trainingIterations);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (dataIn != null) {
+                try {
+                    dataIn.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+    
+    public void classifyNewTweet(String tweet) {
+        DocumentCategorizerME myCategorizer = new DocumentCategorizerME(model);
+        double[] outcomes = myCategorizer.categorize(tweet);
+        String category = myCategorizer.getBestCategory(outcomes);
+
+        if (category.equalsIgnoreCase("1")) {
+            System.out.println("The tweet is positive :) ");
+        } else {
+            System.out.println("The tweet is negative :( ");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
